f7d80d0da144599105134729137b9fdba36dfd6e
cloudcome <cloudcome@163.com>
2016-04-09 00:07:25 +0800

    init

---
+++ test/app-main.js
@@ -0,0 +1,15 @@
+/**
+ * 入口
+ * @author ydr.me
+ * @create 2016-04-09 00:04
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+    
+    module.exports = {
+        circle: require('../src/circle.js'),
+        square: require('../src/square.js')
+    };
+});
